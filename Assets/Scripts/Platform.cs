d23a9ed09e446d14b6e58ca6ebcaddefb842f04c
Sidnei <xplouses@gmail.com>
2019-09-18 13:24:22 -0300

    Moviments added to platform

---
+++ Assets/Scripts/Platform.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+    public float MovimentVelocity = 13.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            GetComponent<Transform>().position 
+                += Vector3.left * MovimentVelocity * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            GetComponent<Transform>().position 
+                += Vector3.right * MovimentVelocity * Time.deltaTime;
+        }
+    }
+}
